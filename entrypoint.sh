9ad57d2bb96f7a142b8e6a4ac77e0185e1043618
RumenMitrev <57098783+rumenmitrev@users.noreply.github.com>
2022-02-03 01:36:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,8 @@ git fetch upstream
 git checkout master
 
 echo "merging ......"
-git merge --allow-unrelated-histories upstream/master -v -m "fetch upstream" --commit
+#git merge --allow-unrelated-histories upstream/master -v -m "fetch upstream" --commit
+git merge upstream/master -v
 
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
